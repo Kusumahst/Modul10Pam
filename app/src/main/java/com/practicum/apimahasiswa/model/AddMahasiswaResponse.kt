af1c5b9bdb60c08774c2fd923d934cf226bddae8
Muhammad Helga <muhammadhelga41@gmail.com>
2025-05-14 14:32:34 +0700

    restAPI

---
+++ app/src/main/java/com/practicum/apimahasiswa/model/AddMahasiswaResponse.kt
@@ -0,0 +1,10 @@
+package com.practicum.apimahasiswa.model
+
+import com.google.gson.annotations.SerializedName
+
+class AddMahasiswaResponse {
+    @SerializedName("message")
+    val message: String? = null
+    @SerializedName("status")
+    val isStatus = false
+}
